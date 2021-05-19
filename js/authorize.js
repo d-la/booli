727e455dc8197c195adc0c07ba351f910b52f41e
Daniel La <la.danieln@gmail.com>
2021-05-19 13:30:39 -0400

    goodbye

--- js/authorize.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-        if(req.isAuthenticated()) {
-            return next();
-        }
-        req.flash('error_msg', 'Please Log In to View This Page');
-        res.redirect('/login');
-    }
-};
